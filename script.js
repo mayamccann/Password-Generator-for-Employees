b5221cc466c6ec424f0e86bd2258e814223dd1b1
Maya McCann <mccannmaya@gmail.com>
2023-07-05 19:05:17 -0400

    created if statements for variables

--- script.js
+++ script.js
@@ -13,9 +13,24 @@ function generatePassword() {
     alert("Please choose an appropriate length for your password.")
     return
   }
-  var uppercase = confirm("Do you want uppercase letters in your password?");
-  var lowercase = confirm("Would you like to use lowercase letters in your password?");
+  var uppercase = confirm ("Do you want uppercase letters in your password?");
+  var lowercase = confirm ("Would you like to use lowercase letters in your password?");
   var specialcharacters = confirm ("Do you want special characters in your password?");
   var numbers = confirm ("Would you like to have numbers in your password?");
 
+  if (uppercase === true) {
+    passwordoptions=passwordoptions.concat(uppercaseletters)
+   }
+   
+   if (lowercase === true) {
+     passwordoptions=passwordoptions.concat(lowercaseletter)
+    }
+ 
+    if (specialcharacters === true) {
+     passwordoptions=passwordoptions.concat(specialcharacter)
+    }
+ 
+    if (numbers === true) {
+     passwordoptions=passwordoptions.concat(numbs)
+    } 
 }
