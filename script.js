4126fe965e67a56bb51d611ee35b7310b296767b
Maya McCann <mccannmaya@gmail.com>
2023-07-05 19:01:02 -0400

    Add function for generating password

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("")
 var lowercaseletter = "abcdefghijklmnopqrstuvwxyz".split("")
 var specialcharacter = "!@$#%^&*()_?".split("")
 var numbs = "0123456789".split("")
+
+function generatePassword() {
+ var passwordoptions = []
+ let newPassword = []
+ var passwordlength = prompt("How long did you want your password to be?");
+ if (passwordlength<8 || passwordlength>128){
+    alert("Please choose an appropriate length for your password.")
+    return
+  }
+}
