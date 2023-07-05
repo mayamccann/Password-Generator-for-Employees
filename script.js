0d93bfe1c7d994e8335b0afc26ea629d114e29a7
Maya McCann <mccannmaya@gmail.com>
2023-07-05 19:08:44 -0400

    password to password input

--- script.js
+++ script.js
@@ -47,3 +47,11 @@ function generatePassword() {
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
