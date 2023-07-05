6aba1df6ce5d4b0f8d618c8e4171a9333b42e96b
Maya McCann <mccannmaya@gmail.com>
2023-07-05 18:51:23 -0400

    Add variables : letters, numbers and special characters

---
+++ script.js
@@ -0,0 +1,6 @@
+// Assignment code here
+
+var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("")
+var lowercaseletter = "abcdefghijklmnopqrstuvwxyz".split("")
+var specialcharacter = "!@$#%^&*()_?".split("")
+var numbs = "0123456789".split("")
