b050e1b4054028bf6dd0f8a81480c972dd7540e5
Maya McCann <mccannmaya@gmail.com>
2023-07-05 19:03:58 -0400

    Adding more questions for the user

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ var numbs = "0123456789".split("")
 function generatePassword() {
  var passwordoptions = []
  let newPassword = []
- var passwordlength = prompt("How long did you want your password to be?");
+ var passwordlength = prompt("How long do you want your password to be?");
  if (passwordlength<8 || passwordlength>128){
     alert("Please choose an appropriate length for your password.")
     return
   }
+  var uppercase = confirm("Do you want uppercase letters in your password?");
+  var lowercase = confirm("Would you like to use lowercase letters in your password?");
+  var specialcharacters = confirm ("Do you want special characters in your password?");
+  var numbers = confirm ("Would you like to have numbers in your password?");
+
 }
